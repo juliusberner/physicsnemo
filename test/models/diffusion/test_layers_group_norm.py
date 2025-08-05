c5e80102ac92261a6b0c357c66f6e42dc456131b
Charlelie Laurent <claurent@nvidia.com>
2025-08-04 18:29:08 -0700

    Added non-regression test for GroupNorm
    Signed-off-by: Charlelie Laurent <claurent@nvidia.com>

--- test/models/diffusion/test_layers_group_norm.py
+++ test/models/diffusion/test_layers_group_norm.py
@@ -14,23 +14,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# from pathlib import Path
-
+# from physicsnemo.models.diffusion.layers import GroupNorm, get_group_norm
+import common
 import pytest
 import torch
 
 import physicsnemo
 from physicsnemo.models.diffusion.layers import GroupNorm
 
-# from physicsnemo.models.diffusion.layers import GroupNorm, get_group_norm
-
 
 def _instantiate_model(cls, seed: int = 0, **kwargs):
     """
     Helper function to instantiate a model with reproducible random parameters.
     """
-    model = cls(**kwargs)
-    gen = torch.Generator(device="cpu")
+    model: physicsnemo.Module = cls(**kwargs)
+    gen: torch.Generator = torch.Generator(device="cpu")
     gen.manual_seed(seed)
     with torch.no_grad():
         for param in model.parameters():
@@ -63,7 +61,7 @@ class GroupNormModule(physicsnemo.Module):
                 eps=1e-3,
             )
 
-    factory = classmethod(_instantiate_model)
+    factory: classmethod = classmethod(_instantiate_model)
 
     def forward(self, x):
         return self.group_norm(x)
@@ -75,80 +73,53 @@ def generate_data(device: str):
     """
     torch.manual_seed(0)
     B, C_in, H, W = 2, 64, 8, 16
-    x = torch.randn(B, C_in, H, W).to(device)
+    x: torch.Tensor = torch.randn(B, C_in, H, W).to(device)
     return x
 
 
 @pytest.mark.parametrize("device", ["cuda:0", "cpu"])
 def test_group_norm_non_regression(device):
     """
-    Test that GroupNorm can be instantiated and compare the output with a reference
-    implementation.
+    Test that GroupNorm can be instantiated and compare the output with a
+    reference output generated with v1.0.1.
     """
-    model = GroupNormModule.factory().to(device)
-    x = generate_data(device)
-    out = model(x)
-    assert out.shape == x.shape
+
+    for test_params in (
+        {
+            "arch_type": "GN_type_1",
+        },
+        {
+            "arch_type": "GN_type_2",
+        },
+    ):
+        err_msg: str = (
+            f"Failed with: {', '.join(f'{k}={v}' for k, v in test_params.items())}"
+        )
+
+        model: GroupNormModule = GroupNormModule.factory(
+            arch_type=test_params["arch_type"]
+        ).to(device)
+
+        # Check that the model is instantiated correctly
+        if test_params["arch_type"] == "GN_type_1":
+            assert model.group_norm.num_groups == 16, err_msg
+            assert model.group_norm.weight.shape == (64,), err_msg
+            assert model.group_norm.bias.shape == (64,), err_msg
+            assert model.group_norm.eps == 1e-5, err_msg
+        elif test_params["arch_type"] == "GN_type_2":
+            assert model.group_norm.num_groups == 2, err_msg
+            assert model.group_norm.weight.shape == (64,), err_msg
+            assert model.group_norm.bias.shape == (64,), err_msg
+            assert model.group_norm.eps == 1e-3, err_msg
+
+        x: torch.Tensor = generate_data(device)
+        out: torch.Tensor = model(x)
+        assert common.validate_accuracy(
+            out,
+            file_name=f"group_norm_{test_params['arch_type']}-v1.0.1.pth",
+        ), err_msg
 
 
-# @pytest.mark.parametrize("device", ["cuda:0", "cpu"])
-# def test_unet_block_load_checkpoint(device):
-#     """
-#     Test that UNetBlock can be loaded from a checkpoint generated with v1.0.1.
-#     and test non-regression on the output.
-#     """
-#     torch.manual_seed(0)
-
-#     file_name = str(
-#         Path(__file__).parents[1].resolve()
-#         / Path("data")
-#         / Path("unet_block_with_attention-v1.0.1.mdlus")
-#     )
-
-#     # model_0 generated with seed=0 from v1.0.1 checkpoint
-#     model_0 = UNetBlockModuleWithAttention.from_checkpoint(
-#         file_name=file_name,
-#     ).to(device)
-
-#     # TODO: add test with use_apex_gn=True once it's fixed
-#     for test_params in (
-#         {
-#             "use_apex_gn": False,
-#             "fused_conv_bias": False,
-#         },
-#         {
-#             "use_apex_gn": False,
-#             "fused_conv_bias": True,
-#         },
-#     ):
-#         err_msg = (
-#             f"Failed with: {', '.join(f'{k}={v}' for k, v in test_params.items())}"
-#         )
-
-#         # model_1 generated with seed=0 from fresh model, should be the same as model_0
-#         model_1 = UNetBlockModuleWithAttention.factory(
-#             seed=0,
-#             use_apex_gn=test_params["use_apex_gn"],
-#             fused_conv_bias=test_params["fused_conv_bias"],
-#         ).to(device)
-#         # model_1.save("unet_block_with_attention-v1.0.1.mdlus")
-#         # model_2 generated with seed=1 from fresh model, should be different from model_0
-#         model_2 = UNetBlockModuleWithAttention.factory(
-#             seed=1,
-#             use_apex_gn=test_params["use_apex_gn"],
-#             fused_conv_bias=test_params["fused_conv_bias"],
-#         ).to(device)
-
-#         x, emb = generate_data(device)
-
-#         out_0 = model_0(x, emb)
-#         out_1 = model_1(x, emb)
-#         out_2 = model_2(x, emb)
-
-#         assert torch.allclose(out_0, out_1, atol=1e-3), err_msg
-#         assert not torch.allclose(out_0, out_2, atol=1e-3), err_msg
-
-#         # after loading the state_dict of model_0, model_2 should be the same as model_0
-#         model_2.load_state_dict(model_0.state_dict())
-#         out_2 = model_2(x, emb)
-#         assert torch.allclose(out_0, out_2, atol=1e-3), err_msg
+# TODO: add test to make sure we can load checkpoint generated with v1.0.1 and
+# load it with a Module implemented with get_group_norm instead of GroupNorm.
+# (Note: it needs to use the _overridable_args)
